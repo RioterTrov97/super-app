5b9a33525ad6fd519d9a4a76c3c8ad864ec811f9
trovtle basnet <basnet.trovtle@gmail.com>
2021-04-25 15:17:43 +1000

    New Changes

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,53 @@
-import express from "express"
-import connectDB from './config/db.js'
-import path from 'path'
-import colors from 'colors'
-import dotenv from 'dotenv'
-import adminRoutes from './routes/adminRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import partnerRoutes from './routes/partnerRoutes.js'
+import express from 'express';
+import connectDB from './config/db.js';
+import path from 'path';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import adminRoutes from './routes/adminRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import partnerRoutes from './routes/partnerRoutes.js';
 // import apiroutes from './apiRoute'
-import listRoutes from './routes/listRoutes.js'
-import logRoutes from './routes/logRoutes.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
-import uploadRoutes from './routes/uploadRoutes.js'
+import listRoutes from './routes/listRoutes.js';
+import logRoutes from './routes/logRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
+dotenv.config();
 
-dotenv.config()
+const app = express();
 
+connectDB();
 
-const app = express()
-
-
-
-connectDB()
-
-app.use(express.json())
+app.use(express.json());
 
 // app.use('/api', apiroutes)
 
-app.use('/api/admins', adminRoutes)
-app.use('/api/partners', partnerRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/lists', listRoutes)
-app.use('/api/upload', uploadRoutes)
-app.use('/api/logs', logRoutes)
+let PORT = process.env.PORT || 4000;
 
-app.use("*", (req, res, next) =>{
-    res.status(404).json({
-        message: "Not Found"
-    })
-})
+app.use('/api/admins', adminRoutes);
+app.use('/api/partners', partnerRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/lists', listRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/api/logs', logRoutes);
+
+app.use('*', (req, res, next) => {
+	res.status(404).json({
+		message: 'Not Found',
+	});
+});
 
 const __dirname = path.resolve();
-app.use('/upload', express.static(path.join(__dirname, '/supersupport-node/uploads')));
+app.use(
+	'/upload',
+	express.static(path.join(__dirname, '/supersupport-node/uploads'))
+);
 
+app.use(notFound);
+app.use(errorHandler);
 
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
+});
 
-app.use(notFound)
-app.use(errorHandler)
-
-
-// app.listen(PORT, () => {
-// console.log(`listening on port ${PORT}`)
-// })
-
-export default app 
+export default app;
