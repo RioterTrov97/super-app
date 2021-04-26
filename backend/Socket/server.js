1e25b6d6d51da2b18997bb5d9fb0cc6d271f4ddf
RioterTrov97 <63705894+RioterTrov97@users.noreply.github.com>
2021-04-26 15:38:09 +1000

    Add files via upload

--- backend/Socket/server.js
+++ backend/Socket/server.js
@@ -1,68 +1,45 @@
-import app from '../index.js';
-import Admin from '../models/AdminModel.js';
 import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
 import { Server } from 'socket.io';
-import { createServer } from 'http';
 
-const httpServer = createServer(app);
+let io;
 
-const io = new Server(httpServer, {
-	cors: {
-		origin: '*',
-	},
-});
-
-io.use(async (socket, next) => {
-	try {
-		const token = socket.handshake.query.token;
-		const payload = await jwt.verify(token, 'Nepali');
-		socket.adminId = payload.id;
-		next();
-	} catch (err) {}
-});
-
-let adminRoomList = [];
-
-io.on('connection', (socket) => {
-	console.log('Connected: ' + socket.adminId);
-
-	socket.on('disconnect', () => {
-		console.log('Disconnected: ' + socket.adminId);
+const init = async (httpServer) => {
+	io = new Server(httpServer, {
+		cors: {
+			origin: '*',
+		},
 	});
 
-	socket.on('joinRoom', (callRoomId) => {
-		socket.join(callRoomId);
-		console.log('An admin joined callroom: ' + callRoomId);
+	io.use(async (socket, next) => {
+		try {
+			const token = socket.handshake.query.token;
+			const payload = await jwt.verify(token, 'Nepali');
+			socket.adminId = payload.id;
+			next();
+		} catch (err) {}
 	});
 
-	socket.on('leaveRoom', (callRoomId) => {
-		socket.leave(callRoomId);
-		console.log('An admin left callroom: ' + callRoomId);
+	io.on('connection', (socket) => {
+		console.log('Connected: ' + socket.adminId);
+
+		socket.join(socket.adminId);
+
+		socket.on('disconnect', () => {
+			console.log('Disconnected: ' + socket.adminId);
+		});
+
+		socket.on('call', async (data) => {
+			io.to(socket.adminId).emit('newCall', data);
+		});
 	});
+};
 
-	socket.on('joinCallRoom', (callRoomId) => {
-		socket.join(callRoomId);
-		adminRoomList.push(callRoomId);
-		console.log(`callRoomId`, callRoomId);
-		console.log('An admin joined Admincallroom: ' + callRoomId);
-	});
+const getIO = () => {
+	if (io == null) {
+		console.log('IO not found');
+	} else {
+		return io;
+	}
+};
 
-	socket.on('leaveCallRoom', (callRoomId) => {
-		socket.leave(callRoomId);
-		adminRoomList = adminRoomList.filter((item) => item !== callRoomId);
-		console.log(`callRoomId`, callRoomId);
-		console.log('An admin left Admincallroom: ' + callRoomId);
-	});
-
-	socket.on('call', async (roomId, data) => {
-		console.log(roomId, data);
-		io.to(adminRoomList[0]).emit('newCall', data);
-	});
-});
-
-const PORT = process.env.PORT || 4000;
-
-httpServer.listen(PORT, () => {
-	console.log(`listening on port ${PORT}`);
-});
+export { init, getIO };
